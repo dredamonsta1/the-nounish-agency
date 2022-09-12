8c3feb502edcd350e5df9b0f4a262d71be92242a
Andre <andre_w3@yahoo.com>
2022-09-12 00:55:15 -0400

    adding about component

--- the-nounish-agency/src/components/navBar/NavBar.js
+++ the-nounish-agency/src/components/navBar/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NavBar.css";
+import About from "../about/About";
 
 
 
@@ -36,22 +37,10 @@ function NavBar() {
                 Fifth heading
                 This is some placeholder content for the scrollspy page. Note that as you scroll down the page, the appropriate navigation link is highlighted. It's repeated throughout the component example. We keep adding some more example copy here to emphasize the scrolling and highlighting.</p>
                 <h4 id="scrollspyHeading2">About</h4>
-                <p>First heading
-                This is some placeholder content for the scrollspy page. Note that as you scroll down the page, the appropriate navigation link is highlighted. It's repeated throughout the component example. We keep adding some more example copy here to emphasize the scrolling and highlighting.
-        
-                Second heading
-                This is some placeholder content for the scrollspy page. Note that as you scroll down the page, the appropriate navigation link is highlighted. It's repeated throughout the component example. We keep adding some more example copy here to emphasize the scrolling and highlighting.
-        
-                Third heading
-                This is some placeholder content for the scrollspy page. Note that as you scroll down the page, the appropriate navigation link is highlighted. It's repeated throughout the component example. We keep adding some more example copy here to emphasize the scrolling and highlighting.
-        
-                Fourth heading
-                This is some placeholder content for the scrollspy page. Note that as you scroll down the page, the appropriate navigation link is highlighted. It's repeated throughout the component example. We keep adding some more example copy here to emphasize the scrolling and highlighting.
-        
-                Fifth heading
-                This is some placeholder content for the scrollspy page. Note that as you scroll down the page, the appropriate navigation link is highlighted. It's repeated throughout the component example. We keep adding some more example copy here to emphasize the scrolling and highlighting.</p>
+                <About />
+               
                 <h4 id="scrollspyHeading3">Third heading</h4>
-                <p>...</p>
+                <About />
                 <h4 id="scrollspyHeading4">Fourth heading</h4>
                 <p>...</p>
                 <h4 id="scrollspyHeading5">Fifth heading</h4>
