729ecd4c69baf691bda3f889059c14e2ab4e445d
Andre <andre_w3@yahoo.com>
2022-09-27 02:28:55 -0400

    working on landing page

--- the-nounish-agency/src/components/home/Home.js
+++ the-nounish-agency/src/components/home/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import './Home.css';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 
 
@@ -6,13 +8,14 @@ import React from "react";
 function Home(){
     
     return(
-            <div className="home-func">
+        <div className="home-func">
      
             
             
             <div className="home-jtron">
                 <h1>hype</h1>
             </div>
+
             <p>First heading
                 This is some placeholder content for the scrollspy page. Note that as you scroll down the page, the appropriate navigation link is highlighted. It's repeated throughout the component example. We keep adding some more example copy here to emphasize the scrolling and highlighting.
         
--- the-nounish-agency/src/components/navBar/NavBar.js
+++ the-nounish-agency/src/components/navBar/NavBar.js
@@ -30,7 +30,7 @@ function NavBar() {
 
 
             <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example bg-light p-3 rounded-2" tabindex="0">
-                <h4 id="scrollspyHeading1">Home</h4>
+                <h4 id="scrollspyHeading1" className="home-section"></h4>
                 <Home />
                 
                 <h4 id="scrollspyHeading2">About</h4>
